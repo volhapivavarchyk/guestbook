9b4c0e6d3707202f660a14ad483a91adaeb95d4f
Volha Pivovarchyk <pivovarchyk@tut.by>
2019-07-29 00:51:31 +0300

    added the abstract class

--- app/guestbook/app/controllers/image/JpegImage.php
+++ app/guestbook/app/controllers/image/JpegImage.php
@@ -5,55 +5,14 @@ namespace Piv\Guestbook\App\Controllers\Image;
 
 class JpegImage implements ImageInterface
 {
-    protected $image;
 
-    public function __construct($image)
+    public function createImageFromFile(string $filename): resource
     {
-        $this->image = $image;
+        return imagecreatefromjpeg($filename);
     }
 
-    public function createImage()
+    public function createFileFromImage(resource $new_image, string $path, string $filename): void
     {
-      $filename = Config::DIR_PUBLIC . "upload/temp/" . $fileImg->getClientFilename();
-      $fileImg->moveTo($filename);
-      $this->resizeAndMoveImage($fileImg, $filename, Config::DIR_PUBLIC . "upload/img/", 320, 240);
-      $this->resizeAndMoveImage($fileImg, $filename, Config::DIR_PUBLIC . "upload/img/small/", 60,
-          50);
-      unlink($filename);
-    }
-
-    protected function resizeAndMoveImage(
-        UploadedFile $fileImg,
-        string $filename,
-        string $path,
-        int $max_width,
-        int $max_height
-    ): void {
-        list($width, $height, $type) = getimagesize($filename);
-        if (($width > $max_width) || ($height > $max_height)) {
-            $w_index = $max_width / $width;
-            $h_index = $max_height / $height;
-            $new_width = $w_index > $h_index ? $width * $h_index : $width * $w_index;
-            $new_height = $w_index > $h_index ? $height * $h_index : $height * $w_index;
-        } else {
-            $new_width = $width;
-            $new_height = $height;
-        }
-
-        $new_image = imagecreatetruecolor((int)$new_width, (int)$new_height);
-        $image = imagecreatefromjpeg($filename);
-        imagecopyresampled(
-            $new_image,
-            $image,
-            0,
-            0,
-            0,
-            0,
-            (int)$new_width,
-            (int)$new_height,
-            $width, $height
-        );
         imagejpeg($new_image, $path . $fileImg->getClientFilename());
     }
-
 }
