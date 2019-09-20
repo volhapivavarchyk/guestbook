3aede803377ad4b9a0059841358ffa364f93e1bf
Volha <pivovarchyk@tut.by>
2019-09-20 17:25:28 +0300

    changed

--- app/guestbook/public/index.php
+++ app/guestbook/public/index.php
@@ -1,11 +1,23 @@
 <?php
 
 use Symfony\Component\HttpFoundation\{Request, Response};
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Piv\Guestbook\Src\Kernel;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
-$kernel = new Kernel();
+
+$dispatcher = new EventDispatcher();
+// ... добавить какие-то слушатели событий
+$dispatcher->addSubscriber(new )
+
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+$kernel = new Kernel($dispatcher, $controllerResolver, new RequestStack(), $argumentResolver);
+
 $response = $kernel->handle($request);
 $response->send();
+
+$kernel->terminate($request, $response);
